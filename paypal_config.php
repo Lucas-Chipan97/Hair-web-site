1e8788bbdb9b50ac1334372e8c9207c83661f0d1
lucas chipan <lucas.chipan97@gmail.com>
2025-03-24 14:42:55 +0100

    New page

--- paypal_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('PAYPAL_BUSINESS_EMAIL', 'lucas.bibi49@gmail.com');
-define('PAYPAL_MERCHANT_ID', '5YLRBAA4EQT4N');
-define('PAYPAL_RETURN_URL', 'http://votre_domaine.com/confirm.php');
-define('PAYPAL_CANCEL_URL', 'http://votre_domaine.com/index.html');
-define('PAYPAL_NOTIFY_URL', 'http://votre_domaine.com/ipn_listener.php');
-?>
